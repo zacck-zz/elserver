c169603e1ce26114dd983ad562b3a29a8c0325bd
Zacck Osiemo <coderv63@gmail.com>
2017-10-20 10:35:30 +0200

    Add a static route for the request Generate a resp_body, Updated the map with the contents of the resp_body Use Map contents for response

--- lib/elserver/handler.ex
+++ lib/elserver/handler.ex
@@ -14,22 +14,24 @@ defmodule Elserver.Handler do
       |> String.split("\n") 
       |> List.first
       |> String.split(" ")
-    %{ method: method, path: path, resp_body: "" } #last expression of a variable is implicitly returned
+    %{ method: method, path: path, resp_body: "" }  
   end
-
+  
+  #last expression of a variable is implicitly returned
+ 
   def route(conv) do
-    # TODO: Create a new map that also has the response body:
-    conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
+    #Create a new map that also has the response body:
+    %{ conv | resp_body: "Baboons, Trees, Eland" }
   end
 
   def format_response(conv) do
-    # TODO: Use values in the map to create an HTTP response string:
+    # Use values in the map to create an HTTP response string:
     """
     HTTP/1.1 200 OK
     Content-Type: text/html
-    Content-Length: 20
+    Content-Length: #{String.length(conv.resp_body)}
 
-    Bears, Lions, Tigers
+    #{conv.resp_body}
     """
   end
 
