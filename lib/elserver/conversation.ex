d430dd7349c39f33fa1211190666082aeb78af04
Zacck Osiemo <coderv63@gmail.com>
2017-10-24 10:23:00 +0200

    move struct functions to struct module

--- lib/elserver/conversation.ex
+++ lib/elserver/conversation.ex
@@ -1,3 +1,23 @@
 defmodule Elserver.Conversation do
   defstruct method: "", path: "", resp_body: "", status: nil
+  
+  def full_status(conv) do 
+    "#{conv.status} #{status_reason(conv.status)}"
+  end 
+
+  # define a private function using defp codes 
+  defp status_reason(code) do 
+    %{
+      200 => "OK",
+      201 => "Created",
+      202 => "Accepted",
+      204 => "No Content",
+      401 => "Unauthorized",
+      403 => "Forbidden", 
+      404 => "Not Found",
+      500 => "Internal Server Error", 
+    }[code]
+  end 
+
+
 end 
--- lib/elserver/handler.ex
+++ lib/elserver/handler.ex
@@ -58,27 +58,13 @@ defmodule Elserver.Handler do
   def format_response(%Conversation{} = conv) do
     # Use values in the map to create an HTTP response string:
     """
-    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP/1.1 #{Conversation.full_status(conv)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
   end
-
-  # define a private function using defp codes 
-  defp status_reason(code) do 
-    %{
-      200 => "OK",
-      201 => "Created",
-      202 => "Accepted",
-      204 => "No Content",
-      401 => "Unauthorized",
-      403 => "Forbidden", 
-      404 => "Not Found",
-      500 => "Internal Server Error", 
-    }[code]
-  end 
 
 end 
 request = """
