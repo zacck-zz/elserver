0651b916a29b91e71c83a7393247c3030bb3c342
Zacck Osiemo <coderv63@gmail.com>
2017-10-24 10:26:27 +0200

    update parser test to use conversation struct

--- test/elserver/handler_test.exs
+++ test/elserver/handler_test.exs
@@ -1,9 +1,11 @@
 defmodule Elserver.HandlerTest do
   use ExUnit.Case
+  alias Elserver.Conversation 
+
 
   test "it should return a 404 on paths that don't exist" do
-    conversation = %{method: "Get", path: "/colors", status: nil,  resp_body: ""}
-    assert Elserver.Handler.route(conversation) == %{method: "Get", path: "/colors", resp_body: "The path #{conversation.path} was not found on this server", status: 404} 
+    conversation = %Conversation{method: "Get", path: "/colors", status: nil,  resp_body: ""}
+    assert Elserver.Handler.route(conversation) == %Conversation{method: "Get", path: "/colors", resp_body: "The path #{conversation.path} was not found on this server", status: 404} 
   end 
 
 end 
