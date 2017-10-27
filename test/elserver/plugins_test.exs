355360e0c25ab3699e12b6a22c88b7883b550472
Zacck Osiemo <coderv63@gmail.com>
2017-10-27 15:30:04 +0200

    work on test for wildlife path rewrite

--- test/elserver/plugins_test.exs
+++ test/elserver/plugins_test.exs
@@ -2,23 +2,32 @@ defmodule Elserver.PluginsTest do
  use ExUnit.Case 
 
   import ExUnit.CaptureIO 
-  alias Elserver.Conversation 
+  alias Elserver.Conversation
+  alias Elserver.Plugins 
   
   test "it should rewrite a path with url parameters" do
     conversation =  %Conversation{method: "Get", path: "/mice?id=1", status: nil, resp_body: ""}
-    assert Elserver.Plugins.rewrite_path(conversation) == %Conversation{conversation | path: "/mice/1"}
+    assert Plugins.rewrite_path(conversation) == %Conversation{conversation | path: "/mice/1"}
   end 
 
+  
   test "the tracker should return an unmodified conversation with 404 status" do 
     conversation = %Conversation{method: "Get", path: "/colors", resp_body: "The path /colors was not found on this server", status: 404} 
-    assert Elserver.Plugins.track(conversation) == conversation
+    assert Plugins.track(conversation) == conversation
     assert capture_io(fn() -> Elserver.Plugins.track(conversation) end) == "Warning: /colors does not exist on this server\n"
   end
+
+  test "/wildlife path should be rewritten to /wildthings" do
+    conversation = %Conversation{method: "Get", path: "/wildlife", resp_body: "The path /colors was not found on this server", status: 404} 
+    assert Plugins.rewrite_path(conversation) == %Conversation{method: "Get", path: "/wildthings", resp_body: "The path /colors was not found on this server", status: 404} 
+ 
+  end 
+
 
   test "the tracker should return unmodified conversation with 200 status" do
     conversation = %Conversation{method: "Get", path: "/colors", resp_body: "The path /colors was not found on this server", status: 200}
 
-   assert Elserver.Plugins.track(conversation) == conversation 
+   assert Plugins.track(conversation) == conversation 
  
   end   
 
