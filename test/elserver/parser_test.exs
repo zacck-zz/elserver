b635dbb88510ca9ef6e9e6b16fd34b0495448244
Zacck Osiemo <coderv63@gmail.com>
2017-10-27 14:51:26 +0200

    add test for url encoded params

--- test/elserver/parser_test.exs
+++ test/elserver/parser_test.exs
@@ -11,8 +11,22 @@ defmodule Elserver.ParserTest do
       """
 
       assert Elserver.Parser.parse(request) == %Conversation{method: "Get", headers: %{"Accept" =>  "*/*", "Host" =>  "example.com", "User-Agent" => "ExampleBrowser/1.0"}, path: "/wildthings", resp_body: "", status: nil}
-  end
+    end
 
+  test "it should parse url encoded params " do 
+    request = """
+    Post /sharks/ HTTP/1.1
+    Host: example.com 
+    User-Agent: ExampleBrowser/1.0
+    Accept: */*
+    Content-Type: application/x-www-form-urlencoded 
+    Content-Length: 16
 
+    name=Great&type=white
+    """
+
+    assert Elserver.Parser.parse(request).params ==  %{"name" => "Great", "type" =>  "white"}
  
+  end 
+
 end 
