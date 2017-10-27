5c360f458919ed72644faae9028c4b080cc8d702
Zacck Osiemo <coderv63@gmail.com>
2017-10-27 15:06:13 +0200

    add test for response formatter

--- test/elserver/handler_test.exs
+++ test/elserver/handler_test.exs
@@ -6,6 +6,20 @@ defmodule Elserver.HandlerTest do
   test "it should return a 404 on paths that don't exist" do
     conversation = %Conversation{method: "Get", path: "/colors", status: nil,  resp_body: ""}
     assert Elserver.Handler.route(conversation) == %Conversation{method: "Get", path: "/colors", resp_body: "The path #{conversation.path} was not found on this server", status: 404} 
-  end 
+  end
+
+  test "it should format a response correctly" do 
+    conversation = %Conversation{method: "Get", path: "/colors", status: 200,  resp_body: "Ok Cool"}
+
+    response = """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: 7
+
+    Ok Cool
+    """
+    assert Elserver.Handler.format_response(conversation) == response
+
+  end  
 
 end 
