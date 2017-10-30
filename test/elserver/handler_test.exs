e0c4da5d0725d0325b12859aa4ee27fdc347952a
Zacck Osiemo <coderv63@gmail.com>
2017-10-30 11:58:04 +0200

    add test for api calls to return json

--- test/elserver/handler_test.exs
+++ test/elserver/handler_test.exs
@@ -4,6 +4,33 @@ defmodule Elserver.HandlerTest do
   
   alias Elserver.Conversation 
 
+  test "it should handle api calls with json" do 
+    request = """
+    Get /api/sharks HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    \r
+    """
+
+    response = Elserver.Handler.handle(request)
+
+    expected_response = """
+    HTTP/1.1 200 OK\r
+    Content-Type: application/json\r
+    Content-Length: 394\r
+    \r
+    [{"type":"HammerHead Shark","name": "Bonzo", "id": 1, "hibernating": false},
+     {"type":"Wobegonng","name":"Mud", "id":2, "hibernating": false},
+     {"type":"Great White","name":"Pablo", "id":3, "hibernating": false},
+     {"type":"Smooth Hound","name": "Ochoa", "id": 4 , "hibernating":false},
+     {"type":"Thresher Shark","name":"Diana", "id":5, "hibernating":false}, 
+     {"type":"Sand Shark","name":"Alex", "id":6, "hibernating":false}
+    ]  
+    """
+
+    assert remove_whitespace(response) == remove_whitespace(expected_response)
+  end 
 
   test "it should handle calls" do 
     request = """
@@ -58,6 +85,10 @@ defmodule Elserver.HandlerTest do
 
     assert Elserver.Handler.route(conversation) == %Conversation{method: "Get", path: "/shark/1", resp_body: "<h1>Show Shark</h1>\n<p>\nIs Bonzo hibernating? <strong>false</strong>\n</p>\n", status: 200}
   
+  end
+
+  def remove_whitespace(text) do
+   String.replace(text, ~r{\s}, "")
   end 
 
 end 
