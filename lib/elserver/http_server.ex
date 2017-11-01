89f3fab34350f41a3d3abdf13042cdf8eb799b9a
Zacck Osiemo <coderv63@gmail.com>
2017-11-01 10:01:46 +0200

    work on using separate process to handle each request

--- lib/elserver/http_server.ex
+++ lib/elserver/http_server.ex
@@ -34,7 +34,8 @@ defmodule Elserver.HttpServer do
     IO.puts "⚡️  Connection accepted!\n"
 
     # Receives the request and sends a response over the client socket.
-    serve(client_socket)
+    # run this in separate background process
+    spawn (fn -> serve(client_socket) end)
 
     # Loop back to wait and accept the next connection.
     accept_loop(listen_socket)
@@ -45,6 +46,7 @@ defmodule Elserver.HttpServer do
   sends a response back over the same socket.
   """
   def serve(client_socket) do
+    IO.puts "#{inspect self()}: is processing a request!"
     client_socket
     |> read_request
     |> Elserver.Handler.handle
