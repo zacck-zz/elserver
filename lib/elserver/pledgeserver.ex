39c36b5f9e63e067ceb543953a0cf036a7ee3a60
Zacck Osiemo <coderv63@gmail.com>
2017-11-06 06:50:47 +0200

    start using processes to listen for messages

--- lib/elserver/pledgeserver.ex
+++ lib/elserver/pledgeserver.ex
@@ -1,14 +1,33 @@
 defmodule Elserver.PledgeServer do 
 
   #persist data
-  def create_pledge(name, amount) do 
-    {:ok, id} = database_pledge(name, amount)
+  #def create_pledge(name, amount) do 
+  # {:ok, id} = database_pledge(name, amount)
     # persist some state
+    #  end 
+
+    #def recent_pledges() do
+    
+    #end
+
+  #use a process to continously monitor for a message 
+  def listen_loop(state) do
+    IO.puts "Waiting for a message"
+
+    receive do
+      {:create_pledge, name, amount} ->  
+        {:ok, id} = database_pledge(name, amount)
+        current_state =  [{name, amount} | state]
+        IO.puts "#{name} plegded #{amount}"
+        IO.puts "The state is now #{inspect(new_state)}"
+        listen_loop(current_state)
+      {sender, :recent_plegdes} ->
+        send sender, {:response, state}
+        listen_loop(state) 
+    end 
   end 
 
-  def recent_pledges() do
-    
-  end
+  
 
   defp database_pledge(_name, _amount) do 
     {:ok, "pledge-#{:rand.uniform(1000)}"}
