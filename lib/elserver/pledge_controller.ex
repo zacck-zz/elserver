b81ab45fd440c8f3445db4683fc811d79813860f
Zacck Osiemo <coderv63@gmail.com>
2017-11-12 05:18:27 +0200

    Flesh out 	Pledge controller to use PledgeServer 	Pledge Server to be stateful

--- lib/elserver/pledge_controller.ex
+++ lib/elserver/pledge_controller.ex
@@ -1,14 +1,15 @@
 defmodule Elserver.PledgeController do 
+  
   def create(conv, %{"name" =>  name, "amount" =>  amount}) do
     #send pledge to persistenent
-    create_pledge(name, String.to_integer(amount))
+    Elserver.PledgeServer.create_pledge(name, String.to_integer(amount))
 
     %{ conv  | status: 201, resp_body: "Thank you #{name} for pledging #{amount}" }
   end
 
   def index(conv) do
     #get some recent pledges from cache
-    pledges = recent_pledges 
+    pledges = Elserver.PledgeServer.recent_pledges 
     %{ conv | status: 200, resp_body: (inspect pledges)}
   end 
 end 
--- lib/elserver/pledgeserver.ex
+++ lib/elserver/pledgeserver.ex
@@ -1,35 +1,59 @@
 defmodule Elserver.PledgeServer do 
 
-  #persist data
-  #def create_pledge(name, amount) do 
-  # {:ok, id} = database_pledge(name, amount)
-    # persist some state
-    #  end 
+  @procname :pledge_server
 
-    #def recent_pledges() do
-    
-    #end
+  # Client Process  
+  def start() do
+    pid = spawn(__MODULE__, :listen_loop, [[]])
+    Process.register(pid, @procname)
+    pid
+  end 
+  
+  def create_pledge( name, amount) do 
+    send @procname, {:create_pledge, name, amount}
+  end
 
+  def recent_pledges do
+    send @procname, {self(), :recent_pledges}
+
+    receive do {:response, pledges} -> pledges end
+  end 
+
+  def total_pledged do
+    send @procname, {self(), :total_pledged}
+
+    receive do {:response, total} -> total end
+  end  
+
+  #Server Process
+  
   #use a process to continously monitor for a message 
   def listen_loop(state) do
-    IO.puts "Waiting for a message"
 
     receive do
-      {:create_pledge, name, amount} ->  
+      {sender, :create_pledge, name, amount} ->  
         {:ok, id} = database_pledge(name, amount)
-        current_state =  [{name, amount} | state]
-        IO.puts "#{name} plegded #{amount}"
-        IO.puts "The state is now #{inspect(new_state)}"
+        recent_state = Enum.take(state, 2)
+        current_state =  [{name, amount} | recent_state]
+        send sender, {:response, id}
         listen_loop(current_state)
-      {sender, :recent_plegdes} ->
+      {sender, :recent_pledges} ->
         send sender, {:response, state}
         listen_loop(state) 
+      {sender, :total_pledged} ->
+        total = &Enum.map(state, elem(&1, 1)) |> Enum.sum 
+        send sender, {:total_pledged, total}
+        listen_loop(state)
+       unexpected -> 
+        inspect unexpected
+        listen_loop(state)
     end 
-  end 
+  end
+
+  defp  database_pledge(_name, _amount) do
+    {:ok, "pledge-#{:rand.uniform(1000)}"} 
+  end  
 
   
 
-  defp database_pledge(_name, _amount) do 
-    {:ok, "pledge-#{:rand.uniform(1000)}"}
-  end 
 end 
