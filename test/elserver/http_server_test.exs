f3ceaf2f5873e961b59fe5d451c146120d2d485f
Zacck Osiemo <coderv63@gmail.com>
2017-11-12 06:09:42 +0200

    add tests for concurrent requests on the server

--- test/elserver/http_server_test.exs
+++ test/elserver/http_server_test.exs
@@ -6,10 +6,24 @@ defmodule Elserver.HttpTest do
     port = 4000
     spawn(HttpServer, :start, [port])
     
-    {:ok, response} = HTTPoison.get "http://localhost:#{port}/wildthings" 
+    parent = self()
 
-    assert response.body == "Baboons, Trees, Eland, Sharks"
-    assert response.status_code  == 200
+    max_concurrent  = 5 
+
+    for _ <- 1..max_concurrent do
+      spawn(fn -> 
+        {:ok, response} = HTTPoison.get "http://localhost:#{port}/wildthings" 
+        send(parent, {:ok, response})
+      end)
+    end 
+
+    for _ <- 1..max_concurrent do
+      receive do
+        {:ok, response} ->
+          assert response.body == "Baboons, Trees, Eland, Sharks"
+          assert response.status_code  == 200
+      end
+    end 
 
   end
 
