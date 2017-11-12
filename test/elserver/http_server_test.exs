2172960f6109e7664afa1eba91571f18fa6d2fc9
Zacck Osiemo <coderv63@gmail.com>
2017-11-12 06:01:24 +0200

    use httppoison as client for testing httpserver

--- test/elserver/http_server_test.exs
+++ test/elserver/http_server_test.exs
@@ -1,30 +1,15 @@
 defmodule Elserver.HttpTest do
   use ExUnit.Case 
-  alias Elserver.HttpClient 
   alias Elserver.HttpServer
 
   test "server shold accept connection and respond" do 
     port = 4000
     spawn(HttpServer, :start, [port])
-
-    request = """
-    GET /wildthings HTTP/1.1\r
-    Host: example.com\r
-    User-Agent: ExampleBrowser/1.0\r
-    Accept: */*\r
-    \r
-    """
     
-    response = HttpClient.response(port, request)
+    {:ok, response} = HTTPoison.get "http://localhost:#{port}/wildthings" 
 
-    expected_response = """
-    HTTP/1.1 200 OK\r
-    Content-Type: text/html\r
-    Content-Length: 29\r
-    \r
-    Baboons, Trees, Eland, Sharks
-    """
-    assert response == expected_response
+    assert response.body == "Baboons, Trees, Eland, Sharks"
+    assert response.status_code  == 200
 
   end
 
