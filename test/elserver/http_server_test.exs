ddc586139cc0dae7ae36665d680bed58732e90f2
Zacck Osiemo <coderv63@gmail.com>
2017-11-16 08:44:19 +0200

    change httpserver test to use tasks

--- test/elserver/http_server_test.exs
+++ test/elserver/http_server_test.exs
@@ -5,26 +5,18 @@ defmodule Elserver.HttpTest do
   test "server shold accept connection and respond" do 
     port = 4000
     spawn(HttpServer, :start, [port])
-    
-    parent = self()
 
-    max_concurrent  = 5 
+    url = "http://localhost:#{port}/wildthings"
 
-    for _ <- 1..max_concurrent do
-      spawn(fn -> 
-        {:ok, response} = HTTPoison.get "http://localhost:#{port}/wildthings" 
-        send(parent, {:ok, response})
-      end)
-    end 
-
-    for _ <- 1..max_concurrent do
-      receive do
-        {:ok, response} ->
-          assert response.body == "Baboons, Trees, Eland, Sharks"
-          assert response.status_code  == 200
-      end
-    end 
-
+    1..5 
+    |> Enum.map(fn(_) -> Task.async(fn -> HTTPoison.get(url) end) end) #run a task for each number to get the url 
+    |> Enum.map(&Task.await/1) # for each task await a result 
+    |> Enum.map(&assert_successful_response/1)
   end
+
+  defp assert_successful_response({:ok, resp}) do
+    assert resp.status_code == 200
+    assert resp.body == "Baboons, Trees, Eland, Sharks"
+  end 
 
 end
