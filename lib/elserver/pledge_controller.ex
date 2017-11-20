c0fa9dd2cf5f0160ceff196132780f6fac1a2aa5
Zacck Osiemo <coderv63@gmail.com>
2017-11-20 13:17:55 +0200

    add views to render form for app and list of pledges

--- lib/elserver/pledge_controller.ex
+++ lib/elserver/pledge_controller.ex
@@ -1,15 +1,21 @@
 defmodule Elserver.PledgeController do 
-  
+
+  import Elserver.View, only: [render: 3]
+
+
   def create(conv, %{"name" =>  name, "amount" =>  amount}) do
     #send pledge to persistenent
     Elserver.PledgeServer.create_pledge(name, String.to_integer(amount))
-
     %{ conv  | status: 201, resp_body: "Thank you #{name} for pledging #{amount}" }
   end
+
+  def new(conv) do 
+    render(conv, "new_pledge.eex", [])
+  end 
 
   def index(conv) do
     #get some recent pledges from cache
     pledges = Elserver.PledgeServer.recent_pledges 
-    %{ conv | status: 200, resp_body: (inspect pledges)}
+    render(conv, "pledges.eex", pledges: pledges)
   end 
 end 
