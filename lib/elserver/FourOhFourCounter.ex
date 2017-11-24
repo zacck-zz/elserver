6892c88fc384d2dac7951c16d5d2d09a77668b24
Zacck Osiemo <coderv63@gmail.com>
2017-11-24 07:50:40 +0200

    convert 404 counter to use a Generic server

--- lib/elserver/FourOhFourCounter.ex
+++ lib/elserver/FourOhFourCounter.ex
@@ -1,49 +1,57 @@
 defmodule Elserver.FourOhFourCounter do 
   @counter :counter
 
+
   def start() do
-    pid = spawn(__MODULE__, :counter_loop, [[]]) 
-    Process.register(pid, @counter)
-    pid 
+    Elserver.GenericServer.start(__MODULE__, [], @counter)
   end 
 
   def bump_count(path) do   
-    send @counter, {self(), :bump_count, path}
-
-    receive do {:response, count} -> count end
+    call @counter, {:bump_count, path}
   end
 
   def get_count(path) do 
-    send @counter, {self(), :get_count, path}
-
-    receive do {:response, count} -> count end
+    call @counter, {:get_count, path}
   end
 
   def get_counts do 
-    send @counter, {self(), :get_counts}
+    call @counter, :get_counts
+  end
 
-    receive do {:response, counts} -> counts end
-  end 
+  def call(pid, message) do
+    send pid, {:call, self(), message}
 
+    receive do {:response, response} ->  response end 
+  end
 
 
   # Server interface
   def counter_loop(state) do
     receive do
-      {sender, :bump_count, path} ->
-        current_state = [path | state]
-        send sender, {:response, Enum.count(current_state)}
-        counter_loop(current_state)
-      {sender, :get_count, path} -> 
-      send sender, {:response, Enum.count(state, fn(x) ->  x == path end)}
-        counter_loop(state)
-      {sender, :get_counts} -> 
-        totals =  Enum.chunk_by(Enum.sort(state), &(&1))
-        counts = Enum.reduce(totals, %{}, &(Map.put(&2, Enum.at(&1, 0), Enum.count(&1))))
-        send sender, {:response, counts}
-        counter_loop(state)
+      {:call, sender, message} when is_pid(sender) ->
+        {response, new_state} = handle_call(message, state)
+        send sender, {:response, response}
+        counter_loop(new_state)
       unexpected ->
         counter_loop(state)
     end  
+  end
+
+  def handle_call({:bump_count, path}, state) do
+    current_state = [path | state]
+    {:ok, current_state}
   end 
+
+  def handle_call({:get_count,path}, state) do
+    count =  Enum.count(state, fn(x) -> x == path end)
+    {count, state}
+  end 
+
+  def handle_call(:get_counts, state) do
+    totals =  Enum.chunk_by(Enum.sort(state), &(&1))
+    counts = Enum.reduce(totals, %{}, &(Map.put(&2, Enum.at(&1, 0), Enum.count(&1))))
+    {counts, state}
+  end 
+
+   
 end 
