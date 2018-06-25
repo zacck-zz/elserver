15bf13c17fa32d8fec74d71966f1fe837cb9a29f
Zacck Osiemo <zacck@users.noreply.github.com>
2018-06-25 16:09:47 +0200

    Add comments to conversation

--- lib/elserver/conversation.ex
+++ lib/elserver/conversation.ex
@@ -1,4 +1,11 @@
 defmodule Elserver.Conversation do
+  @moduledoc """
+  This module contains the structure for our Conversation object
+  It enables us to serialize informations into a Conversation and 
+  also enables use to deserialize a Conversation into information 
+  """
+  
+  # build a struct 
   defstruct method: "", 
             path: "", 
             params: %{},
@@ -7,10 +14,17 @@ defmodule Elserver.Conversation do
             status: nil,
             resp_headers: %{"Content-Type" => "text/html"}
   
+  @doc"""
+  This function takes a conversation and returns the full 
+  reason containing code and the status
+  """
   def full_status(conv) do 
     "#{conv.status} #{status_reason(conv.status)}"
   end 
-
+  
+  @doc"""
+  This function calculates the size of the response body 
+  """
   def content_length(conv) do 
     byte_size(conv.resp_body)
   end
